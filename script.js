bcaba10587c476e0fbb5a66ba0c0ce61a9f12527
Gabriel Guerreiro <bielguerreiro2526@gmail.com>
2025-10-12 20:42:54 -0300

    alteração nas áread e cadastros

--- script.js
+++ script.js
@@ -843,6 +843,7 @@ document.addEventListener("DOMContentLoaded", function () {
       containerGeral.style.height = `${paginaAtiva.scrollHeight + 60}px`;
     }
   }
+
   menuItens.forEach((item) => {
     item.addEventListener("click", function () {
       if (isAnimating) return;
